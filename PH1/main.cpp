04bad737f94a1fe18d0a552eee09e00afe9be9bc
Catarina Machado <catarinamachado11@gmail.com>
2019-03-01 13:14:05 +0000

    Add planes

--- PH1/main.cpp
+++ PH1/main.cpp
@@ -14,11 +14,11 @@ void circle(MonadWindow reference,int points){
 	unmkMonadWindow(mon);
 }
 
-float lin( float dh, float h){
+float lin(float dh, float h){
 	return (h - dh)/h;
 }
 
-float cuple( float dh, float h ){
+float cuple(float dh, float h){
 	return sqrt( (h*h  - dh*dh) )/h;
 }
 
@@ -50,12 +50,105 @@ void plane(MonadWindow reference) {
     monadPoint(nw, 0.0, 0.0, 1.0);
     monadPoint(nw, 1.0, 0.0, 1.0);
     monadPoint(nw, 0.0, 0.0, 0.0);
+
+    unmkMonadWindow(nw);
+}
+
+void planeXZ(MonadWindow reference, float lengthX, float lenghtY, float lenghtZ) {
+    MonadWindow nw = mkMonadWindow(reference);
+    monadTranslate(nw, -lengthX/2, 0.0, -lenghtZ/2);
+
+    monadPoint(nw, lengthX, 0.0, 0.0);
+    monadPoint(nw, 0.0, 0.0, 0.0);
+    monadPoint(nw, lengthX, 0.0, lenghtZ);
+
+    monadPoint(nw, 0.0, 0.0, lenghtZ);
+    monadPoint(nw, lengthX, 0.0, lenghtZ);
+    monadPoint(nw, 0.0, 0.0, 0.0);
+
+    unmkMonadWindow(nw);
+}
+
+void planeXY(MonadWindow reference, float lengthX, float lengthY, float lenghtZ) {
+    MonadWindow nw = mkMonadWindow(reference);
+    monadTranslate(nw, -lengthX/2, 0.0, -lenghtZ/2);
+
+    monadPoint(nw, lengthX, 0.0, 0.0);
+    monadPoint(nw, 0.0, 0.0, 0.0);
+    monadPoint(nw, lengthX, lengthY, 0.0);
+
+    monadPoint(nw, 0.0, lengthY, 0.0);
+    monadPoint(nw, lengthX, lengthY, 0.0);
+    monadPoint(nw, 0.0, 0.0, 0.0);
+}
+
+void planeYZ(MonadWindow reference, float lengthX, float lengthY, float lenghtZ) {
+    MonadWindow nw = mkMonadWindow(reference);
+    monadTranslate(nw, -lengthX/2, 0.0, -lenghtZ/2);
+
+    monadPoint(nw, 0.0, lengthY, 0.0);
+    monadPoint(nw, 0.0, 0.0, 0.0);
+    monadPoint(nw, 0.0, lengthY, lenghtZ);
+
+    monadPoint(nw, 0.0, 0.0, lenghtZ);
+    monadPoint(nw, 0.0, lengthY, lenghtZ);
+    monadPoint(nw, 0.0, 0.0, 0.0);
+}
+
+void planeMod(MonadWindow reference, float lengthX, float lengthY, float lenghtZ, int nx, int ny, int nz) {
+    MonadWindow nw = mkMonadWindow(reference);
+    monadTranslate(nw, 0.0, 0.0, 0.0);
+
+    monadPoint(nw, 0.0, lengthY, 0.0);
+    monadPoint(nw, 0.0, 0.0, 0.0);
+    monadPoint(nw, 0.0, lengthY, lenghtZ);
+
+    monadPoint(nw, 0.0, 0.0, lenghtZ);
+    monadPoint(nw, 0.0, lengthY, lenghtZ);
+    monadPoint(nw, 0.0, 0.0, 0.0);
+}
+
+void box(MonadWindow reference, float x, float y, float z, int divisions) {
+    MonadWindow nw = mkMonadWindow(reference);
+    monadTranslate(nw, -0.5, 0.0, -0.5);
+
+    monadRotate(nw, 180, 1.0, 0.0, 1.0);
+    planeXZ(nw, x, y, z); //face tem que estar ao contrario
+
+    monadTranslate(nw, 0.0, y, 0.0);
+    planeXZ(nw, x, y, z);
+    monadTranslate(nw, 0.0, -y, 0.0);
+
+    planeXY(nw, x, y, z);
+
+    monadTranslate(nw, 0.0, 0.0, z); //face tem que estar ao contrario
+    planeXY(nw, x, y, z);
+    monadTranslate(nw, 0.0, 0.0, -z);
+
+    planeYZ(nw, x, y, z);
+
+    monadTranslate(nw, x, 0.0, 0.0); //face tem que estar ao contrario
+    planeYZ(nw, x, y, z);
+    monadTranslate(nw, -x, 0.0, 0.0);
+
+    /*
+     *  nr de divisoes = nr de planos - 1
+
+
+    for(int i = 0; i < divisions - 1; i++) {
+        //planosXZ
+        double novoX = x / (divisions - 1);
+        double novoY = y / (divisions - 1);
+
+        planeXZ(nw, y, z);
+    }*/
+
 }
 
 int main(int argc, char **argv) {
 	MonadWindow reference = mkMonadWindow();
-    plane(reference);
-	print_trace(reference,"figure.xml","figure");
+    box(reference, 2.0, 2.0, 2.0, 0);
+    print_trace(reference,"figure.xml","figure");
 
 	unmkMonadWindow(reference);
 	return 1;
