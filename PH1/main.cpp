d581847834a305e068aae85fcd84c37831a56e61
Goncalo-Faria <gonca2372@gmail.com>
2019-03-02 11:26:50 +0000

    ff

--- PH1/main.cpp
+++ PH1/main.cpp
@@ -44,7 +44,8 @@ int main(int argc, char **argv) {
 
     //box(reference, 2.0,2.0,2.0,2);
 
-    frameStacker(reference,3, 1, cil);
+    //frameStacker(reference,3, 3, cuple);
+    sphere(reference,1.0,10,10);
     //frameCircle(reference,6);
 
     frameTrace(reference,"figure.xml","figure");
